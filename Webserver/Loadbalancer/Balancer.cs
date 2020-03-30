9d51b8dd1f90328f30ca0b9277107640910ab269
Conor Groenenberg <0965749@hr.nl>
2020-03-30 03:02:55 +0200

    Added balancer database connection Renamed Startup.cs to Balancer.cs

--- Webserver/Loadbalancer/Balancer.cs
+++ Webserver/Loadbalancer/Balancer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using Database.SQLite;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -15,6 +19,8 @@ namespace Webserver.LoadBalancer
 		public static IPEndPoint MasterEndpoint { get; set; }
 
 		public static ConcurrentDictionary<IPEndPoint, ServerProfile> Servers { get; } = new ConcurrentDictionary<IPEndPoint, ServerProfile>();
+
+		public static SQLiteAdapter Database { get; private set; }
 
 		private static bool isMaster;
 		/// <summary>
@@ -53,6 +59,8 @@ namespace Webserver.LoadBalancer
 			if (multicastAddress == null) multicastAddress = IPAddress.Parse("224.0.0.1"); //Default multicast address;
 			Port = port;
 
+			Database = new SQLiteAdapter("Database.db");
+
 			//Create a client and send a discover message.
 			var client = Networking.GetClient(addresses, multicastAddress, balancerPort);
 			client.Client.ReceiveTimeout = 1000;
@@ -72,14 +80,14 @@ namespace Webserver.LoadBalancer
 					try
 					{
 						response = JObject.Parse(Encoding.UTF8.GetString(RawResponse));
-						if (response == null || !response.ContainsKey("Type"))
+						if (response == null || !response.ContainsKey("$type"))
 							continue;
 					}
 					catch (JsonReaderException)
 					{
 						continue;
 					}
-					if ((string)response["Type"] == "MASTER")
+					if ((string)response["$type"] == "MASTER")
 					{
 						IsMaster = false;
 						break;
@@ -100,12 +108,66 @@ namespace Webserver.LoadBalancer
 			}
 			client.Close();
 
+			// Set up database query events
+			Program.Database.Inserting += OnDatabaseInsert;
+
 			//Initialise the networking system
 			MasterEndpoint = address;
 			Networking.Init(addresses, multicastAddress, balancerPort);
 			Console.WriteLine("Local endpoint is {0}, Master endpoint is {1}", Networking.LocalEndPoint, address);
 			Console.Title = $"Local - {Networking.LocalEndPoint} | Master - {address}";
 			return Networking.LocalEndPoint.Address;
+		}
+
+		/// <summary>
+		/// Gets the lock that prevents a slave server from modifying items in the database before
+		/// receiving acknowledgement from the master server.
+		/// </summary>
+		public static Semaphore DatabaseUpdateLock { get; } = new Semaphore(1, 1);
+
+		private static void OnDatabaseInsert(SQLiteAdapter sender, InsertEventArgs args)
+		{
+			if (IsMaster)
+			{
+				// TODO implement this but only at the end of the query
+			}
+			else
+			{
+				var items = new JArray();
+				var json = new JObject()
+				{
+					{ "$destination", MasterEndpoint.ToString() },
+					{ "$type", "QUERY_INSERT" },
+					{ "type", args.ModelType.FullName },
+					{ "items", items }
+				};
+
+				foreach (var item in args.Collection)
+					items.Add(JObject.FromObject(item));
+
+				// Register event for the master's ACK response
+				void swapCollection(object sender, Slave.QUERY_INSERT_ACK_EventArgs e)
+				{
+					// Swaps the elements from these event args with the insert event args
+					for (int i = 0; i < args.Collection.Count; i++)
+						args.Collection[i] = e.Collection[i];
+
+					// Continue the execution of the other event
+					DatabaseUpdateLock.Release();
+				}
+				Slave.QUERY_INSERT_ACK += swapCollection;
+
+				DatabaseUpdateLock.WaitOne();
+				// Send the items to the master in order to have it insert the items and broadcast the results
+				Networking.SendData(json);
+				
+				// Initiate "deadlock" (unlocked later by the QUERY_INSERT ACK from the master server)
+				DatabaseUpdateLock.WaitOne(5000);
+				
+				// Immediately unschedule the local function after reacquiring the lock
+				Slave.QUERY_INSERT_ACK -= swapCollection;
+				DatabaseUpdateLock.Release();
+			}
 		}
 	}
 }
