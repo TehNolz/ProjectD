a20c42d69878f766f60ad1a725a0d8cea864ffbf
TehNolz <36479504+TehNolz@users.noreply.github.com>
2020-06-01 18:04:36 +0200

    Change /account DELETE to use ID instead of email

--- Webserver/API/Endpoints/Account/DeleteAccount.cs
+++ Webserver/API/Endpoints/Account/DeleteAccount.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json.Linq;
-
+using System;
+using System.Linq;
 using System.Net;
 
 using Webserver.Models;
@@ -13,24 +14,25 @@ namespace Webserver.API.Endpoints.Account
 		public override void DELETE()
 		{
 			//Get required fields
-			if (!((JObject)Data).TryGetValue("Email", out string email))
+			if (!((JObject)Data).TryGetValue("ID", out Guid ID))
 			{
 				Response.Send("Missing fields", HttpStatusCode.BadRequest);
 				return;
 			}
 
-			//Cancel if Email is "Administrator", because the built-in Admin shouldn't ever be deleted.
-			if (email == "Administrator")
-			{
-				Response.Send("Can't delete built-in administrator", HttpStatusCode.Forbidden);
-				return;
-			}
+			var account = Database.Select<User>("ID = @ID", new { ID }).FirstOrDefault();
 
 			//Check if the specified user exists. If it doesn't, send a 404 Not Found
-			var account = User.GetByEmail(Database, email);
 			if (account == null)
 			{
 				Response.Send("No such user", HttpStatusCode.NotFound);
+				return;
+			}
+
+			//Cancel if Email is "Administrator", because the built-in Admin shouldn't ever be deleted.
+			if (account.Email == "Administrator")
+			{
+				Response.Send("Can't delete built-in administrator", HttpStatusCode.Forbidden);
 				return;
 			}
 
